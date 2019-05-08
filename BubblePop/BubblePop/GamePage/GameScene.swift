d4f4a996e3cd0c931edf4bdaf480ff80e29a1666
Steven Ding <anluoridge@gmail.com>
2019-05-08 13:06:49 +1000

    score mechanism

--- BubblePop/BubblePop/GamePage/GameScene.swift
+++ BubblePop/BubblePop/GamePage/GameScene.swift
@@ -8,34 +8,56 @@
 
 import SpriteKit
 import GameplayKit
+import Foundation
 
 class GameScene: SKScene {
-    
+    static let scoreBarHeight = CGFloat(120)
+    static let pink = UIColor.init(red: 254/255, green: 127/255, blue: 156/255, alpha: 1) // TODO: UIColor extension
+    private var score = 0
+    var highScore = 0
+    var timeLeft = 60
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
     var bubbles = [SKShapeNode]()
+    var lastColorHash = 0
 //    var timer = Timer()
     
     private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
+    private var scoreLabel : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+
+//    override init(size: CGSize, highScore: Int, time: Int) {
+//        super.init(size: size)
+//        self.highScore =  highScore
+//        self.timeLeft = time
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
 
     override func didMove(to view: SKView) {
         super.didMove(to: view)
+        self.removeAllChildren()
         spawnBubbles()
+        scoreLbl()
+    }
 
-        label = SKLabelNode(text: "0")
-        label?.horizontalAlignmentMode = .center
-        label?.fontColor = .black
+    func scoreLbl() {
+        scoreLabel = SKLabelNode(text: String(self.score))
+        scoreLabel?.fontSize = 40
+        scoreLabel?.horizontalAlignmentMode = .center
+        scoreLabel?.fontColor = .black
+        // TODO: score font
+//        scoreLabel?.fontName = .
         if let sceneWidth = scene?.frame.width {
             if let sceneHeight = scene?.frame.height {
-                label?.position = CGPoint(x: sceneWidth/2, y: sceneHeight - 30)
+                scoreLabel?.position = CGPoint(x: sceneWidth/2, y: sceneHeight - 50)
             }
         }
 //        label?.position = CGPoint(x: scene?.frame.width/2, y: 0)//CGRect(x: 0, y: 0, width: 100, height: 30)
-        self.addChild(label!)
+        self.addChild(scoreLabel!)
     }
-
     override func sceneDidLoad() {
 
         self.lastUpdateTime = 0
@@ -106,6 +128,7 @@ class GameScene: SKScene {
     override func didChangeSize(_ oldSize: CGSize) {
         super.didChangeSize(oldSize)
         self.removeAllChildren()
+        scoreLbl()
     }
 
     func bubbleNode() -> SKShapeNode {
@@ -143,7 +166,7 @@ class GameScene: SKScene {
         case 1...40:
             return .red
         case 41...70:
-            return UIColor.init(red: 254/255, green: 127/255, blue: 156/255, alpha: 1)
+            return GameScene.pink
         case 71...85:
             return .green
         case 86...95:
@@ -159,7 +182,7 @@ class GameScene: SKScene {
         guard let marginTop = scene?.frame.maxY
                 else {return}
 
-        let outY = marginTop + 40
+        let outY = marginTop - GameScene.scoreBarHeight
         let outPoint = CGPoint(x: bubble.position.x, y: outY)
 
 //        let locationInScene = scene!.convertPoint(fromView: destinationPoint)
@@ -186,6 +209,34 @@ class GameScene: SKScene {
         for node in self.children {
             if node.frame.contains(pos) {
                 let bubble = node as! SKShapeNode
+                /*
+                Red	1	40%
+Pink	2	30%
+Green	5	15%
+Blue	8	10%
+Black	10	5%
+                */
+                var scoreEarned = 0.0
+                switch (bubble.fillColor.hash) {
+                case UIColor.red.hash:
+                    scoreEarned = 1
+                case UIColor.blue.hash:
+                    scoreEarned = 8
+                case UIColor.green.hash:
+                    scoreEarned = 5
+                case UIColor.black.hash:
+                    scoreEarned = 10
+                case GameScene.pink.hash:
+                    scoreEarned = 2
+                default: // black grey system
+                    scoreEarned = 0
+                }
+                if bubble.fillColor.hash == lastColorHash {
+                    scoreEarned *= 1.5
+                }
+                self.score += Int(round(scoreEarned))
+                scoreLabel?.text = String(self.score)
+                lastColorHash = bubble.fillColor.hash
                 bubble.removeFromParent()
                 return
             }
